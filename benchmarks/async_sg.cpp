5ba3036198d0aa6f95d350fb362930bec0cb60ce
Alex Constantin-Gómez <alex.constantin-gomez19@imperial.ac.uk>
2023-05-31 16:49:28 +0100

    Update async baseline to support large number of reqs

--- benchmarks/async_sg.cpp
+++ benchmarks/async_sg.cpp
@@ -13,20 +13,29 @@
 
 class ScatterGatherService {
 
+    typedef struct __attribute__((packed)) conn_info  {
+        int      fd;
+        uint16_t type;
+        uint16_t bgid;
+    } conn_info_t;
+
     std::vector<Worker>&       d_workers;
     uint32_t                   d_nextRequest = 0;
     io_uring                   d_ring;
     msghdr*                    d_msgHdrs;
     uint16_t                   d_bgid = 42;
-    char*                      d_buffers;
+    // char*                      d_buffers;
     int                        d_skFd;
+    size_t                     d_numSkReads;
+    std::vector<char*>         d_packetBufferPool;
     constexpr static const int NUM_BUFFERS = std::numeric_limits<uint16_t>::max(); // for fair comparison with sgbpf
 
-    constexpr static uint64_t READ_OP = 0xdeadbeef;
+    constexpr static uint16_t READ_OP = 0x12;
 
 public:
     ScatterGatherService(std::vector<Worker>& workers)
         : d_workers{workers}
+        , d_numSkReads{0}
     {
         std::cout << "Workers loaded: " << workers.size() << std::endl;
         increaseMaxNumFiles();
@@ -34,7 +43,6 @@ public:
         d_skFd = socket(AF_INET, SOCK_DGRAM, 0);
 
         d_msgHdrs = new msghdr[d_workers.size()];
-        d_buffers = new char[sizeof(sg_msg_t) * NUM_BUFFERS];
 
         // Setup io uring
         io_uring_params params;
@@ -44,19 +52,34 @@ public:
             throw std::runtime_error{"Failed to initialise io_uring queue"};
 
         // Preallocate and register buffers to receive the packets in
+        provideBuffers(true);
+    }
+
+    uint16_t provideBuffers(bool immediate = false) {
+        // Register packet buffers for buffer selection
+        char* buffer  = new char[NUM_BUFFERS * sizeof(sg_msg_t)];
+        d_packetBufferPool.push_back(buffer);
+
+        // std::cout << "[DEBUG] Providing buffers to the kernel (new bgid = "
+        //         << d_packetBufferPool.size() - 1 << ") num reads = " << d_numSkReads << std::endl;
+
+        auto bgid = d_packetBufferPool.size() - 1;  // the idx of the buffer in the pool
         io_uring_sqe* sqe = io_uring_get_sqe(&d_ring);
-        io_uring_prep_provide_buffers(sqe, d_buffers, sizeof(sg_msg_t), NUM_BUFFERS, d_bgid, 0);
-        io_uring_submit(&d_ring);
-        io_uring_cqe* cqe;
-        io_uring_wait_cqe(&d_ring, &cqe);
-        if (cqe->res < 0)
-            throw std::runtime_error{"Failed to provide io_uring buffers to the kernel"};
-        io_uring_cqe_seen(&d_ring, cqe);
+        io_uring_prep_provide_buffers(sqe, buffer, sizeof(sg_msg_t), NUM_BUFFERS, bgid, 0);
+
+        if (immediate) {
+            io_uring_submit(&d_ring);
+            io_uring_cqe* cqe;
+            io_uring_wait_cqe(&d_ring, &cqe);
+            if (cqe->res < 0)
+                throw std::runtime_error{"Failed to provide io_uring buffers to the kernel"};
+            io_uring_cqe_seen(&d_ring, cqe);
+        }
+        return bgid;
     }
 
     ~ScatterGatherService() {
         delete[] d_msgHdrs;
-        delete[] d_buffers;
     }
 
     void scatter(const char* msg, size_t len) {
@@ -75,6 +98,7 @@ public:
             .iov_len = sizeof(sg_msg_t),
         };
 
+        uint16_t bgid = d_packetBufferPool.size() - 1; // use the most recent bgid
         for (auto i = 0u; i < d_workers.size(); ++i) {
             auto& worker = d_workers[i];
             memset(&d_msgHdrs[i], 0, sizeof(msghdr));
@@ -89,11 +113,21 @@ public:
             io_uring_sqe_set_flags(sqe, 0);
 
             // Add read
+            if (d_numSkReads == NUM_BUFFERS) {
+                bgid = provideBuffers();
+                d_numSkReads = 0;
+            }
+            d_numSkReads++;
             sqe = io_uring_get_sqe(&d_ring);
             io_uring_prep_recv(sqe, worker.socketFd(), NULL, sizeof(sg_msg_t), 0);
             io_uring_sqe_set_flags(sqe, IOSQE_BUFFER_SELECT);
-            sqe->buf_group = d_bgid;
-            sqe->user_data = READ_OP;
+            sqe->buf_group = bgid;
+            conn_info_t conn_i = {
+                .fd = worker.socketFd(),
+                .type = READ_OP,
+                .bgid = bgid,
+            };
+            memcpy(&sqe->user_data, &conn_i, sizeof(conn_info_t));
         }
 
         // Submit send syscalls as batch and wait for a response from each worker
@@ -114,9 +148,11 @@ public:
             unsigned head;
             io_uring_for_each_cqe(&d_ring, head, cqe) {
                 ++count;
-                if (cqe->user_data == READ_OP) {
+                const auto conn_i = reinterpret_cast<conn_info_t*>(&cqe->user_data);
+                if (conn_i->type == READ_OP) {
+                    auto bgid = conn_i->bgid;
                     auto bid = cqe->flags >> IORING_CQE_BUFFER_SHIFT;
-                    auto resp = (sg_msg_t*) (d_buffers + bid * sizeof(sg_msg_t));
+                    auto resp = (sg_msg_t*) (d_packetBufferPool[bgid] + bid * sizeof(sg_msg_t));
 
                     // Aggregation logic:
                     auto numElems = resp->hdr.body_len / sizeof(DATA_TYPE);
